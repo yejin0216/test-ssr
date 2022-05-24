8f43cb80053a40c81573b5ed8e7698d692c34a6b
yejin kim <yejin0216@gmail.com>
2022-05-24 21:06:50 +0900

    이미지 모듈 적용하기

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,61 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist', // server.js line13 참고
-  },
-  module: {
-    rules: [
-      {
-        test: /.\js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            configFile: path.resolve(__dirname, '.babelrc.client.js'),
+function getConfig(isServer) {
+  return {
+    entry: isServer
+      ? { server: './src/server.js' }
+      : { main: './src/index.js' },
+    output: {
+      filename: isServer ? '[name].bundle.js' : '[name].[chunkhash].js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/dist/',
+    },
+    target: isServer ? 'node' : 'web',
+    externals: isServer ? [nodeExternals()] : [],
+    node: {
+      __dirname: false,
+    },
+    optimization: isServer
+      ? {
+          splitChunks: false,
+          minimize: false,
+        }
+      : undefined,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              configFile: path.resolve(
+                __dirname,
+                isServer ? '.babelrc.server.js' : '.babelrc.client.js',
+              ),
+            },
           },
         },
-      },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({ template: './template/index.html' }),
-  ],
-  mode: 'production',
-};
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              emitFile: isServer ? false : true,
+            },
+          },
+        },
+      ],
+    },
+    plugins: isServer
+      ? []
+      : [
+          new HtmlWebpackPlugin({
+            template: './template/index.html',
+          }),
+        ],
+    mode: 'production',
+  };
+}
+module.exports = [getConfig(false), getConfig(true)];
